bb4beafe47aa8c97eaa5f4fb25a91ca0a798ddf4
Aaron Ding <aaronding@google.com>
2021-05-19 13:18:49 +0800

    Create gs201 device folder baseline
    clone from gs101 sha 9d1c97c2
    
    Bug: 183183095
    Change-Id: Ie7112bdf0aceb33c9e1956aa52e961b670582733

---
+++ device-common.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2020 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include device/google/gs101/device.mk
+
+# Telephony
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.carrierlock.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.carrierlock.xml
+
+# Factory OTA
+-include vendor/google/factoryota/client/factoryota.mk
+
+# Set system properties identifying the chipset
+PRODUCT_VENDOR_PROPERTIES += ro.soc.manufacturer=Google
+PRODUCT_VENDOR_PROPERTIES += ro.soc.model=GS101
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.vendor.testing_battery_profile=0
