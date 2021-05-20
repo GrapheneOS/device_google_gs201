aac7611e6e72794a0034c6442d8e4932322960ad
Pat Tjin <pattjin@google.com>
2021-05-19 19:20:09 -0700

    Disable secure element for bring up
    Disable secure element for bring up since the service is crashing.
    
    Bug: 186836335
    Signed-off-by: Pat Tjin <pattjin@google.com>
    Change-Id: Ia4ba3824a1f6ec44f326aaf4f7f5c607622391ab

--- device-shipping-common.mk
+++ device-shipping-common.mk
@@ -15,16 +15,16 @@
 
 include device/google/gs201/device-common.mk
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.bluetooth.a2dp_offload.supported=true \
-    persist.bluetooth.a2dp_offload.disabled=false \
-    persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac
-
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.bluetooth.a2dp_offload.supported=true \
+#    persist.bluetooth.a2dp_offload.disabled=false \
+#    persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac
+#
 # SecureElement
-PRODUCT_PACKAGES += \
-    android.hardware.secure_element@1.2-service-gto \
-    android.hardware.secure_element@1.2-service-gto-ese2
+#PRODUCT_PACKAGES += \
+#    android.hardware.secure_element@1.2-service-gto \
+#    android.hardware.secure_element@1.2-service-gto-ese2
 
-PRODUCT_COPY_FILES += \
-    device/google/gs201/nfc/libse-gto-hal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libse-gto-hal.conf \
-    device/google/gs201/nfc/libse-gto-hal2.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libse-gto-hal2.conf
+#PRODUCT_COPY_FILES += \
+#    device/google/gs101/nfc/libse-gto-hal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libse-gto-hal.conf \
+#    device/google/gs101/nfc/libse-gto-hal2.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libse-gto-hal2.conf
