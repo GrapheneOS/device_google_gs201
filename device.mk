87b88bb590152a9414320678132b0b55d219c6bc
Nrithya Kanakasabapathy <nrithya@google.com>
2021-06-12 01:31:35 +0000

    Updates to device make files to include the TPU firmware for whitechapel pro
    Bug: 190569662
    
    Signed-off-by: Nrithya Kanakasabapathy <nrithya@google.com>
    Change-Id: I960d7aaafebb377dbcf976381a9dc6edcc01d8c7

--- device.mk
+++ device.mk
@@ -1066,37 +1066,8 @@ PRODUCT_PACKAGES += \
 include hardware/google/pixel/thermal/device.mk
 PRODUCT_PROPERTY_OVERRIDES += persist.vendor.enable.thermal.genl=true
 
-## TPU firmware
-#PRODUCT_PACKAGES += \
-#	edgetpu-abrolhos.fw
-#
-## TPU NN HAL
-#PRODUCT_PACKAGES += \
-#	android.hardware.neuralnetworks@1.3-service-darwinn
-#
-## TPU NN AIDL HAL
-#PRODUCT_PACKAGES += \
-#	android.hardware.neuralnetworks@service-darwinn-aidl
-#
-## TPU logging service
-#PRODUCT_PACKAGES += \
-#	android.hardware.edgetpu.logging@service-edgetpu-logging
-#
-## TPU application service
-#PRODUCT_PACKAGES += \
-#	vendor.google.edgetpu@1.0-service
-#
-## TPU vendor service
-#PRODUCT_PACKAGES += \
-#	vendor.google.edgetpu_vendor_service@1.0-service
-#
-## TPU HAL client library
-#PRODUCT_PACKAGES += \
-#	libedgetpu_client.google
-#
-## TPU metrics logger library
-#PRODUCT_PACKAGES += \
-#	libmetrics_logger
+## TPU packages
+include device/google/gs201/edgetpu/edgetpu.mk
 
 # Connectivity Thermal Power Manager
 PRODUCT_PACKAGES += \
