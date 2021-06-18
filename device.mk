c1538243043578904253708e31290fb9551b5ac0
Wayne Lin <waynewhlin@google.com>
2021-06-18 11:09:06 +0800

    gps: enable gps module in gs201
    Build GPS with RIL due to library dependencies
    
    Bug: 190707050
    Bug: 191119634
    Test: GPS test pass
    Change-Id: I96151b95a8c7a3bf3d9cea340d6f5aaebba8ffde

--- device.mk
+++ device.mk
@@ -742,30 +742,6 @@ PRODUCT_PACKAGES += \
 	libopenvx-opencl
 endif
 
-#GPS_CHIPSET := 47765
-#
-#PRODUCT_COPY_FILES += \
-#	frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml \
-#	device/google/gs201/gnss/${GPS_CHIPSET}/config/gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.xml \
-#	device/google/gs201/gnss/${GPS_CHIPSET}/config/lhd.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/lhd.conf \
-#	device/google/gs201/gnss/${GPS_CHIPSET}/config/scd.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/scd.conf \
-#	device/google/gs201/gnss/${GPS_CHIPSET}/config/gps.cer:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.cer \
-#	device/google/gs201/gnss/${GPS_CHIPSET}/firmware/SensorHub.patch:$(TARGET_COPY_OUT_VENDOR)/firmware/SensorHub.patch
-#
-#PRODUCT_SOONG_NAMESPACES += \
-#	device/google/gs201/gnss/$(GPS_CHIPSET)
-#
-#PRODUCT_PACKAGES += \
-#	android.hardware.gnss@2.1-impl-google \
-#	gps.default \
-#	flp.default \
-#	gpsd \
-#	lhd \
-#	scd \
-#	android.hardware.gnss@2.1-service-brcm
-#PRODUCT_PACKAGES_DEBUG += \
-#	init.gps_log.rc
-
 # Trusty (KM, GK, Storage)
 #$(call inherit-product, system/core/trusty/trusty-storage.mk)
 #$(call inherit-product, system/core/trusty/trusty-base.mk)
@@ -897,6 +873,9 @@ USE_RADIO_HAL_1_6 := true
 
 ifneq ($(BOARD_WITHOUT_RADIO),true)
 $(call inherit-product-if-exists, vendor/samsung_slsi/telephony/common/device-vendor.mk)
+#GPS HAL
+#TODO(b/191417802)
+include device/google/gs101/gnss/device-gnss.mk
 endif
 
 ifeq (,$(filter %_64,$(TARGET_PRODUCT)))
