61592569422e6670a6df1609758e5e2802beea0d
weichinweng <weichinweng@google.com>
2021-08-23 13:46:24 +0800

    Add legacy bluetooth audio hal into audio hal
    Bug: 194607747
    Test: build pass, legacy A2DP pass.
    Change-Id: Ic8c09bcaad57945ffdd58ea5f8136a93350fa9a3

--- device.mk
+++ device.mk
@@ -953,8 +953,8 @@ PRODUCT_PACKAGES += \
 	android.hardware.audio@7.0-impl \
 	android.hardware.audio.effect@7.0-impl \
 	android.hardware.soundtrigger@2.3-impl \
-	vendor.google.whitechapel.audio.audioext@2.0-impl
-#	android.hardware.bluetooth.audio@2.1-impl \
+	vendor.google.whitechapel.audio.audioext@2.0-impl \
+	android.hardware.bluetooth.audio@2.1-impl
 
 #
 ##Audio HAL libraries
@@ -997,8 +997,8 @@ PRODUCT_COPY_FILES += \
 	frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
         frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
 	frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+	frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml
 
-#        frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
 ##Audio soong
 PRODUCT_SOONG_NAMESPACES += \
 	vendor/google/whitechapel/audio/hal \
