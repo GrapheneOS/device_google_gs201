dd48757095ba294906cc13ec311e5753ed579dc2
Carter Hsu <carterhsu@google.com>
2021-11-24 19:20:22 +0800

    audio: update AudioExt to 3.0
    Bug: 206065000
    Test: build pass
    Signed-off-by: Carter Hsu <carterhsu@google.com>
    Change-Id: Ia8decc9cb0962330cb0321b747929efe53f323ba

--- device.mk
+++ device.mk
@@ -918,7 +918,7 @@ PRODUCT_PACKAGES += \
 	android.hardware.audio@7.1-impl \
 	android.hardware.audio.effect@7.0-impl \
 	android.hardware.soundtrigger@2.3-impl \
-	vendor.google.whitechapel.audio.audioext@2.0-impl \
+	vendor.google.whitechapel.audio.audioext@3.0-impl \
 	android.hardware.bluetooth.audio@2.1-impl
 
 #
