4cdfc08f1b15cdb295aef167d812ec904e2a0b0c
Wayne Lin <waynewhlin@google.com>
2021-12-03 12:08:02 +0800

    Move gps.xml to device folder
    Move gps.xml to device folder and maintain them separately
    by project requirement
    
    Bug: 208725716
    Test: build pass
    Change-Id: I10950cdec6ff77a6a3f9b8ba585960e17c21fe8c

--- gnss/device-gnss.mk
+++ gnss/device-gnss.mk
@@ -6,13 +6,11 @@ PRODUCT_COPY_FILES += \
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 	PRODUCT_COPY_FILES += \
 		device/google/gs201/gnss/47765/config/lhd.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/lhd.conf \
-		device/google/gs201/gnss/47765/config/scd.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/scd.conf \
-		device/google/gs201/gnss/47765/config/gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.xml
+		device/google/gs201/gnss/47765/config/scd.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/scd.conf
 else
 	PRODUCT_COPY_FILES += \
 		device/google/gs201/gnss/47765/config/lhd_user.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/lhd.conf \
-		device/google/gs201/gnss/47765/config/scd_user.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/scd.conf \
-		device/google/gs201/gnss/47765/config/gps_user.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.xml
+		device/google/gs201/gnss/47765/config/scd_user.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/scd.conf
 endif
 
 PRODUCT_SOONG_NAMESPACES += \
