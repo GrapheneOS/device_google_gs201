35a79d9a4404de67c81712b868059dbc607ee60f
Nicole Lee <nicoleytlee@google.com>
2021-12-04 11:39:28 +0000

    Revert "device.mk: enable modem always on logging by default"
    This reverts commit 11475b1588120ac8c36af302d7bb2ada8f4c6df6.
    
    Reason for revert: b/208845456 disable modem logs until PCIe can sleep correctly
    
    Change-Id: I86df81c96685c571a3deb478b3b24141f89e0e22

--- device.mk
+++ device.mk
@@ -315,8 +315,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	persist.vendor.sys.silentlog.tcp = "On" \
 	ro.vendor.cbd.modem_removable = "1" \
 	ro.vendor.cbd.modem_type = "s5100sit" \
-	persist.vendor.sys.modem.logging.br_num=5 \
-	persist.vendor.sys.modem.logging.enable=true
+	persist.vendor.sys.modem.logging.br_num=5
 
 # Enable silent CP crash handling
 PRODUCT_PROPERTY_OVERRIDES += \
