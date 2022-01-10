5012cf936844cdf05332ef1ed5cce71bbd8e6265
horngchuang <horngchuang@google.com>
2022-01-10 17:25:19 +0800

    Add camera permission setting files for gs201
    It was removed by ag/14655832 for gs201 bring up
    
    Bug: 213395353
    Test: launch camera okay, we can turn on/off torch by setting menu bar with L10
    Change-Id: If4c7a22de74bf150024b24de7af19d631cb090fc

--- device.mk
+++ device.mk
@@ -553,7 +553,7 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
 	frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml
 
-#PRODUCT_COPY_FILES += \
+PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
 	frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
 	frameworks/native/data/etc/android.hardware.camera.concurrent.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.concurrent.xml \
