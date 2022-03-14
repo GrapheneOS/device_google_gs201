e00a9b20998b1989022712c22ec301055d02791e
Martin Liu <liumartin@google.com>
2022-03-14 22:45:20 +0800

    Disable memcg
    Pixel kernel don't use MEMCG, let's disable it to
    minimize the overhead.
    
    Bug: 224521042
    Test: check cmdline
    Signed-off-by: Martin Liu <liumartin@google.com>
    Change-Id: I60681fb04e4f9a41814eda5988b6c1a0d4fb127c

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -35,6 +35,7 @@ TARGET_CPU_VARIANT_RUNTIME := cortex-a53
 BOARD_KERNEL_CMDLINE += dyndbg=\"func alloc_contig_dump_pages +p\"
 BOARD_KERNEL_CMDLINE += earlycon=exynos4210,0x10A00000 console=ttySAC0,115200 androidboot.console=ttySAC0 printk.devkmsg=on
 BOARD_KERNEL_CMDLINE += cma_sysfs.experimental=Y
+BOARD_KERNEL_CMDLINE += cgroup_disable=memory
 BOARD_BOOTCONFIG += androidboot.boot_devices=14700000.ufs
 
 TARGET_NO_BOOTLOADER := true
