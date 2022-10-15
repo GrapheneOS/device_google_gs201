5b931b611e8d1c73b4de043c9329b1f7e4aabc6d
flawedworld <flawedworld@flawed.world>
2022-10-15 14:11:37 +0100

    use more modern target CPU configuration

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -27,10 +27,10 @@ TARGET_SOC_NAME := google
 USES_DEVICE_GOOGLE_GS201 := true
 
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_VARIANT := cortex-a55
-TARGET_CPU_VARIANT_RUNTIME := cortex-a55
+TARGET_CPU_VARIANT := cortex-a76
+TARGET_CPU_VARIANT_RUNTIME := cortex-a76
 
 # Enable 64-bit for non-zygote.
 ZYGOTE_FORCE_64 := true
@@ -40,11 +40,11 @@ IGNORE_PREFER32_ON_DEVICE := true
 
 # Build the 32 bit targets
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
-TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
+TARGET_2ND_CPU_VARIANT := cortex-a76
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
 
 BOARD_KERNEL_CMDLINE += dyndbg=\"func alloc_contig_dump_pages +p\"
 BOARD_KERNEL_CMDLINE += earlycon=exynos4210,0x10A00000 console=ttySAC0,115200 androidboot.console=ttySAC0 printk.devkmsg=on
