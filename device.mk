c32ac6613feacb04275b1cdc9271eb72df37ad9b
Krzysztof Kosiński <krzysio@google.com>
2022-10-26 00:12:14 +0000

    Set displaycolor platform variable to gs201.
    Bug: 241708558
    Test: presubmit
    Change-Id: I482707e95c4b00b5b18bae18dae9b2cfb666a45e

--- device.mk
+++ device.mk
@@ -828,6 +828,7 @@ PRODUCT_PACKAGES += \
 #PRODUCT_PACKAGES += \
 #	trusty_metricsd
 
+$(call soong_config_set,google_displaycolor,displaycolor_platform,gs201)
 PRODUCT_PACKAGES += \
 	android.hardware.composer.hwc3-service.pixel \
 	libdisplaycolor
