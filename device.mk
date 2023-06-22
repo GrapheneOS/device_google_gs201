bcb959448c75667b725fb6d00510454575fd4c22
Devika Krishnadas <kdevika@google.com>
2023-06-22 08:57:51 +0000

    Use graphics allocator 2
    Bug: 287353739
    Test: atest VtsHalGraphicsMapperV4_0TargetTest
    
    Change-Id: I080045d75273fcf01d9129dee85fdf3c7b801117
    Signed-off-by: Devika Krishnadas <kdevika@google.com>

--- device.mk
+++ device.mk
@@ -507,8 +507,7 @@ PRODUCT_PACKAGES += \
 	com.android.future.usb.accessory
 
 PRODUCT_PACKAGES += \
-	android.hardware.graphics.mapper@4.0-impl \
-	android.hardware.graphics.allocator-V1-service
+	android.hardware.graphics.allocator-V2-service
 
 PRODUCT_PACKAGES += \
 	android.hardware.memtrack-service.pixel \
