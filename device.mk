671e69eef09f6b0bb9d8b537988c6b3baaa99749
Dinesh Yadav <dkyadav@google.com>
2023-07-10 05:48:36 +0000

    [Cleanup] Move gxp sepolicies to gs-common for P22
    Due to cleanup /gs-common/gxp/dump.mk has been changed to
    /gs-common/gxp/gxp.mk
    
    Bug: 288368306
    Change-Id: I2b463a8e696644703b7cedcf73516ffa9459e9f9
    Signed-off-by: Dinesh Yadav <dkyadav@google.com>

--- device.mk
+++ device.mk
@@ -26,7 +26,7 @@ include device/google/gs-common/pixel_metrics/pixel_metrics.mk
 include device/google/gs-common/performance/perf.mk
 include device/google/gs-common/display/dump.mk
 include device/google/gs-common/camera/dump.mk
-include device/google/gs-common/gxp/dump.mk
+include device/google/gs-common/gxp/gxp.mk
 include device/google/gs-common/gps/dump/log.mk
 include device/google/gs-common/radio/dump.mk
 include device/google/gs-common/umfw_stat/umfw_stat.mk
