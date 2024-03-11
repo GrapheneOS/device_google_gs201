b4993872c3a2dbb554680a13b36d936e0624851c
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-03-11 19:10:37 +0200

    change default UsbPortSecurity mode to CHARGING_ONLY_WHEN_LOCKED_AFU

--- usb/usb/Usb.cpp
+++ usb/usb/Usb.cpp
@@ -1318,7 +1318,7 @@ static const int MODE_CHARGING_ONLY_WHEN_LOCKED_AFU = 3;
 static const int MODE_ENABLED = 4;
 
 UsbExt::UsbExt(std::shared_ptr<Usb> usb) : mUsb(usb) {
-    int initialMode = ::android::base::GetIntProperty("persist.security.usb_mode", MODE_ENABLED);
+    int initialMode = ::android::base::GetIntProperty("persist.security.usb_mode", MODE_CHARGING_ONLY_WHEN_LOCKED_AFU);
     ALOGD("initial persist.security.usb_mode: %i", initialMode);
 
     switch (initialMode) {
