169b08ddfcfcc3142b52839331b18243779e4dfc
Nishant Panwar <nishantpanwar@google.com>
2024-06-12 09:21:44 +0000

    Set dexpreopt and dexopt filter for SystemUI
    Bug: 271887637
    Test: Built and flashed on local device
    Flag: build.RELEASE_SYSTEMUI_USE_SPEED_PROFILE
    Change-Id: I49900d45a60f9f4b3bc15a4027751be30f63e272

--- device.mk
+++ device.mk
@@ -1165,12 +1165,19 @@ include device/google/gs201/dumpstate/item.mk
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/gs201/device_framework_matrix_product.xml
 
 # Preopt SystemUI
+ifneq ($(RELEASE_SYSTEMUI_USE_SPEED_PROFILE), true)
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUIGoogle  # For internal
-PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI  # For AOSP
+PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI        # For AOSP
+endif
 
-# Compile SystemUI on device with `speed`.
+# Set on-device compilation mode for SystemUI.
+ifeq ($(RELEASE_SYSTEMUI_USE_SPEED_PROFILE), true)
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.systemuicompilerfilter=speed-profile
+else
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.systemuicompilerfilter=speed
+endif
 
 # Keymint configuration
 PRODUCT_COPY_FILES += \
