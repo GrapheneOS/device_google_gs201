617251a80a389acd7cc84ca6b106d3684e15c0cf
Jim Tang <jimtang@google.com>
2024-10-04 15:48:16 +0800

    Relocate modem_svc_sit to proper places
    Flag: EXEMPT refactor
    Bug: 358485346
    Test: m modem_svc_sit
    Change-Id: Ib47ac55c504eafd32340e66a7286be48f2ea7b48

--- device.mk
+++ device.mk
@@ -959,9 +959,6 @@ USE_EARLY_SEND_DEVICE_INFO := true
 ifneq ($(BOARD_WITHOUT_RADIO),true)
 $(call inherit-product-if-exists, vendor/samsung_slsi/telephony/$(BOARD_USES_SHARED_VENDOR_TELEPHONY)/common/device-vendor.mk)
 
-# modem_svc_sit daemon
-PRODUCT_PACKAGES += modem_svc_sit
-
 # modem logging binary/configs
 PRODUCT_PACKAGES += modem_logging_control
 
