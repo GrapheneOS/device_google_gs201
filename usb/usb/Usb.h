a0a53e14aa70a307ea47c2b8536814e057f24340
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-10-07 18:55:09 +0300

    add locking to UsbExt::setPortSecurityState()
    Client of this API in system_server is single-threaded, but add locking anyway to avoid the implicit
    requirement of a single single-threaded client.

--- usb/usb/Usb.h
+++ usb/usb/Usb.h
@@ -119,7 +119,9 @@ struct UsbExt : public ext::BnUsbExt {
             const shared_ptr<IPortSecurityStateCallback>& in_callback) override;
     int setPortSecurityStateInner(PortSecurityState in_state);
 
+private:
     std::shared_ptr<Usb> mUsb;
+    pthread_mutex_t mPortSecurityLock;
 };
 
 } // namespace usb
