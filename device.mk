08995439430da2b663373ac92d3cd1a56e9709f2
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-11-18 14:05:43 +0200

    use the Natural display color mode by default

--- device.mk
+++ device.mk
@@ -710,7 +710,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.protected_contents=true
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.display_update_imminent_timeout_ms=50
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	persist.sys.sf.native_mode=2
+	persist.sys.sf.color_saturation=1.0
 PRODUCT_COPY_FILES += \
 	device/google/gs201/display/display_colordata_cal0.pb:$(TARGET_COPY_OUT_VENDOR)/etc/display_colordata_cal0.pb
 
