ed380cf63c0f726d3e7bf5c266f734fb8e9c80da
Fred Underwood <underwoodfred@proton.me>
2025-06-14 12:34:40 +1000

    remove PRODUCT_COPY_FILES

--- device.mk
+++ device.mk
@@ -184,16 +184,9 @@ BOARD_USE_CODEC2_AIDL := V1
 ifneq (,$(filter aosp_%,$(TARGET_PRODUCT)))
 DEVICE_MANIFEST_FILE += \
 	device/google/gs201/manifest_media_aosp.xml
-
-PRODUCT_COPY_FILES += \
-	device/google/gs201/media_codecs_aosp_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml
 else
 DEVICE_MANIFEST_FILE += \
 	device/google/gs201/manifest_media.xml
-
-PRODUCT_COPY_FILES += \
-	device/google/gs201/media_codecs_bo_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
-	device/google/gs201/media_codecs_aosp_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_aosp_c2.xml
 endif
 
 DEVICE_MATRIX_FILE := \
