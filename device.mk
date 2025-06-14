59e73d029a13024ebdae749b8379789c05ae873d
Fred Underwood <underwoodfred@proton.me>
2025-06-14 13:26:48 +1000

    remove properties

--- device.mk
+++ device.mk
@@ -320,11 +320,6 @@ include hardware/google/pixel/rebalance_interrupts/rebalance_interrupts.mk
 USE_LEGACY_LOCAL_AUDIO_HAL := false
 USE_XML_AUDIO_POLICY_CONF := 1
 
-# Enable AAudio MMAP/NOIRQ data path.
-PRODUCT_PROPERTY_OVERRIDES += aaudio.mmap_policy=2
-PRODUCT_PROPERTY_OVERRIDES += aaudio.mmap_exclusive_policy=2
-PRODUCT_PROPERTY_OVERRIDES += aaudio.hw_burst_min_usec=2000
-
 # Calliope firmware overwrite
 #PRODUCT_COPY_FILES += \
 	device/google/gs201/firmware/calliope_dram.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/calliope_dram.bin \
