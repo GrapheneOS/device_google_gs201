5c48d1bbee67466b30a0efb46a2b5682854d38d5
Fred Underwood <underwoodfred@proton.me>
2025-06-14 13:25:52 +1000

    remove PRODUCT_PACKAGES

--- device.mk
+++ device.mk
@@ -312,10 +312,6 @@ endif
 # IRQ rebalancing.
 include hardware/google/pixel/rebalance_interrupts/rebalance_interrupts.mk
 
-# PowerStats HAL
-PRODUCT_PACKAGES += \
-	android.hardware.power.stats-service.pixel
-
 #
 # Audio HALs
 #
