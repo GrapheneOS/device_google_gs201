7c06cffb8e4a83d4b41565ab0b4c671a45f6f4de
Fred Underwood <underwoodfred@proton.me>
2025-06-14 13:05:19 +1000

    remove PRODUCT_COPY_FILES

--- disable_contaminant_detection.sh
+++
@@ -1,6 +0,0 @@
-#!/vendor/bin/sh
-
-max77759tcpc_path=/sys/devices/platform/10d60000.hsi2c/i2c-13/13-0025
-if [ -d $max77759tcpc_path ]; then
-  echo 0 > $max77759tcpc_path/contaminant_detection;
-fi
