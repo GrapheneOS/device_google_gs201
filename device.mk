2377c3edf32a2cc79087da4216e4c62587bdf3ec
Fred Underwood <underwoodfred@proton.me>
2025-06-14 14:32:06 +1000

    remove missing PRODUCT_SOONG_NAMESPACES

--- device.mk
+++ device.mk
@@ -539,10 +539,6 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
 	securedpud.slider
 
-# Trusty Metrics Daemon
-PRODUCT_SOONG_NAMESPACES += \
-	vendor/google/trusty/common
-
 PRODUCT_PACKAGES += \
 	trusty_metricsd
 
