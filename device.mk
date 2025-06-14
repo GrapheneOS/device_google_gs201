9291f14a7b5f0700adbcddf5a9c63eac0e16eb53
Fred Underwood <underwoodfred@proton.me>
2025-06-14 13:05:19 +1000

    remove PRODUCT_COPY_FILES

--- device.mk
+++ device.mk
@@ -223,11 +223,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
 	device/google/$(TARGET_BOARD_PLATFORM)/conf/fstab.persist:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.persist \
 
-# Shell scripts
-PRODUCT_COPY_FILES += \
-	device/google/gs201/init.display.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.display.sh \
-	device/google/gs201/disable_contaminant_detection.sh:$(TARGET_COPY_OUT_VENDOR)/bin/hw/disable_contaminant_detection.sh
-
 include device/google/gs-common/insmod/insmod.mk
 
 # Insmod config files
