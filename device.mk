0407c1abe0beb6dff5d4c04319819dddaa7f5416
Fred Underwood <underwoodfred@proton.me>
2025-06-14 13:58:32 +1000

    remove PRODUCT_COPY_FILES

--- device.mk
+++ device.mk
@@ -483,10 +483,6 @@ endif
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
-# WIFI COEX
-PRODUCT_COPY_FILES += \
-	device/google/gs201/wifi/coex_table.xml:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/coex_table.xml
-
 PRODUCT_PACKAGES += hostapd
 PRODUCT_PACKAGES += wpa_supplicant
 PRODUCT_PACKAGES += wpa_supplicant.conf
