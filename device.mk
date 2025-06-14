7f3127eb35f2277567e5491426424ac2432fe431
Fred Underwood <underwoodfred@proton.me>
2025-06-15 06:13:36 +1000

    remove fstab.gs201* files that adevtool can handle

--- device.mk
+++ device.mk
@@ -209,14 +209,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/google/gs201/conf/init.recovery.device.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.gs201.rc
 
-# Fstab files
-# TODO: Review this. fstab.gs201 and fstab.gs201-fips go into vendor partition so are probably defined
-# to leave for adevtool. The vendor_ramdisks probably aren't handled by adevtool. Unsure if these
-# files are needed by the build system.
 PRODUCT_PACKAGES += \
-	fstab.gs201 \
 	fstab.gs201.vendor_ramdisk \
-	fstab.gs201-fips \
 	fstab.gs201-fips.vendor_ramdisk
 
 include device/google/gs-common/insmod/insmod.mk
