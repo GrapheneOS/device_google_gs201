3eb483bcebb8789540672c25fb213982e977b215
Fred Underwood <underwoodfred@proton.me>
2025-06-14 12:37:03 +1000

    remove missing include

--- device.mk
+++ device.mk
@@ -197,9 +197,6 @@ DEVICE_PACKAGE_OVERLAYS += device/google/gs201/overlay
 # This device is shipped with 33 (Android T)
 PRODUCT_SHIPPING_API_LEVEL := 33
 
-# RKP VINTF
--include vendor/google_nos/host/android/hals/keymaster/aidl/strongbox/RemotelyProvisionedComponent-citadel.mk
-
 # Enforce the Product interface
 PRODUCT_PRODUCT_VNDK_VERSION := current
 PRODUCT_ENFORCE_PRODUCT_PARTITION_INTERFACE := true
