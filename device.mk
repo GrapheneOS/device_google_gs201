4f4e3af8632794fdc16c585b1fb1c35c27efa582
Fred Underwood <underwoodfred@proton.me>
2025-06-14 14:28:47 +1000

    remove missing PRODUCT_PACKAGES

--- device.mk
+++ device.mk
@@ -530,15 +530,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 PRODUCT_TAGS += dalvik.gc.type-precise
 
-# Exynos OpenVX framework
-PRODUCT_PACKAGES += \
-		libexynosvision
-
-ifeq ($(TARGET_USES_CL_KERNEL),true)
-PRODUCT_PACKAGES += \
-	libopenvx-opencl
-endif
-
 # Trusty (KM, GK, Storage)
 $(call inherit-product, system/core/trusty/trusty-storage.mk)
 $(call inherit-product, system/core/trusty/trusty-base.mk)
