60f31e494824c7023b268431ec16ea3968885f3c
Fred Underwood <underwoodfred@proton.me>
2025-06-14 13:40:53 +1000

    remove missing PRODUCT_PACKAGES

--- device.mk
+++ device.mk
@@ -363,8 +363,6 @@ PRODUCT_PACKAGES += \
 
 # WideVine modules
 include device/google/gs201/widevine/device.mk
-PRODUCT_PACKAGES += \
-	liboemcrypto \
 
 PANTHER_PRODUCT := %panther
 CHEETAH_PRODUCT := %cheetah
