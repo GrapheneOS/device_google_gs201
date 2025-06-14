be52112c66ea3ba7d6503208655dd57edfcf889c
Fred Underwood <underwoodfred@proton.me>
2025-06-14 15:40:47 +1000

    remove properties

--- device.mk
+++ device.mk
@@ -724,9 +724,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
     frameworks/native/data/etc/android.hardware.device_unique_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.device_unique_attestation.xml
 
-# Call deleteAllKeys if vold detects a factory reset
-PRODUCT_VENDOR_PROPERTIES += ro.crypto.metadata_init_delete_all_keys.enabled?=true
-
 # Hardware Info
 include hardware/google/pixel/HardwareInfo/HardwareInfo.mk
 
