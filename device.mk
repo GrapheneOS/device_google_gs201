1a3432213a3d22aeec5770dbd7912a6a303e15e3
Fred Underwood <underwoodfred@proton.me>
2025-06-15 06:14:33 +1000

    remove conf/fstab.postinstall

--- device.mk
+++ device.mk
@@ -559,10 +559,6 @@ PRODUCT_PACKAGES += \
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# TODO: Review this.
-PRODUCT_COPY_FILES += \
-	device/google/gs201/conf/fstab.postinstall:$(TARGET_COPY_OUT_PRODUCT)/etc/fstab.postinstall
-
 # fastbootd
 PRODUCT_PACKAGES += \
 	android.hardware.fastboot-service.pixel_recovery \
