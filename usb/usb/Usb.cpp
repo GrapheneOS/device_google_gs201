a160922b9f005d550783b49447bf5f4f49ccc939
Dmitry Muhomor <muhomor.dmitry2@gmail.com>
2025-06-21 19:20:59 +0300

    Revert "move dynamic deny_new_usb2 handling to system_server"
    This reverts commit 07c1f9637c05b1b732b670b997b3e03e177bf97d.

--- usb/usb/Usb.cpp
+++ usb/usb/Usb.cpp
@@ -1324,9 +1324,15 @@ int UsbExt::setPortSecurityStateInner(PortSecurityState in_state) {
     string ccToggleEnablePath = mUsb->mI2cClientPath + kCcToggleEnable;
     string dataPathEnablePath = mUsb->mI2cClientPath + kDataPathEnable;
 
+    bool denyNewUsbWriteResult = SetProperty("security.deny_new_usb2",
+                                             in_state == PortSecurityState::ENABLED ? "0" : "1");
+    if (!denyNewUsbWriteResult) {
+        ALOGE("unable to update security.deny_new_usb2 sysprop");
+    }
+
     bool hasPogo = access(kPogoChargingOnly, F_OK) == 0;
 
-    // '&' is used instead of '&&' to avoid short-circuit evaluation
+    // '&' is used instead of '&&' intentionally to disable short-circuit evaluation
 
     switch (in_state) {
         case PortSecurityState::DISABLED: {
@@ -1362,6 +1368,11 @@ int UsbExt::setPortSecurityStateInner(PortSecurityState in_state) {
             return IUsbExt::ERROR_FILE_WRITE;
         }
     }
+
+    if (!denyNewUsbWriteResult) {
+        return IUsbExt::ERROR_DENY_NEW_USB_WRITE;
+    }
+
     return IUsbExt::NO_ERROR;
 }
 
@@ -1383,10 +1394,6 @@ UsbExt::UsbExt(std::shared_ptr<Usb> usb) : mUsb(usb) {
             break;
         case MODE_CHARGING_ONLY_WHEN_LOCKED_AFU:
         case MODE_ENABLED:
-            // deny_new_usb2 is enabled by init before loading the USB driver module
-            if (!SetProperty("security.deny_new_usb2", "0")) {
-                ALOGE("unable to set security.deny_new_usb2 sysprop");
-            }
             setPortSecurityStateInner(PortSecurityState::ENABLED);
             break;
     }
