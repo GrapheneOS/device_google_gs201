9ba1c8feed127f7228ca805a823d8df7b522d244
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-06-28 14:58:12 +0300

    add privapp-permissions for high brightness mode manager

--- device-common.mk
+++ device-common.mk
@@ -31,6 +31,9 @@ PRODUCT_COPY_FILES += \
     device/google/gs201/product-permissions-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product-permissions-stock.xml
 endif
 
+PRODUCT_COPY_FILES += \
+    device/google/gs201/system_ext-permissions-stock.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/system_ext-permissions-stock.xml
+
 # Android Verified Boot
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
