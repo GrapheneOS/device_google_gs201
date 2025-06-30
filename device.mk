9d416c7a59a3ceecfebff1b464c58046545c8a68
Fred Underwood <underwoodfred@proton.me>
2025-07-01 08:00:21 +1000

    strip usb/

--- device.mk
+++ device.mk
@@ -272,10 +272,8 @@ endif
 PRODUCT_PACKAGES += android.hardware.sensors-service.multihal
 
 # USB HAL
-PRODUCT_PACKAGES += \
-	android.hardware.usb-service
-PRODUCT_PACKAGES += \
-	android.hardware.usb.gadget-service
+PRODUCT_COPY_FILES += \
+    device/google/gs201/usb/usb/android.hardware.usb-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.usb-service.rc
 
 # MIDI feature
 PRODUCT_COPY_FILES += \
