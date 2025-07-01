b19b03be8244e23adb96455d0f6f9d3623005bff
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-07-01 10:46:17 +0000

    strip usb, update and keep modified rc file only

--- device.mk
+++ device.mk
@@ -271,11 +271,8 @@ endif
 # Add sensor HAL AIDL product packages
 PRODUCT_PACKAGES += android.hardware.sensors-service.multihal
 
-# USB HAL
-PRODUCT_PACKAGES += \
-	android.hardware.usb-service
-PRODUCT_PACKAGES += \
-	android.hardware.usb.gadget-service
+# USB HAL rc file
+PRODUCT_PACKAGES += usb_service_init_rc
 
 # MIDI feature
 PRODUCT_COPY_FILES += \
